f34e0d4385d276a2c8ee13d93b80b583d6ac74f6
chithraep <epchithra910@gmail.com>
2023-02-13 21:50:23 +0530

    js file added

---
+++ covid 19/archive/web site initial/quote.js
@@ -0,0 +1,10 @@
+function generate(){
+    var quotes = {
+        "-Albert Einstein" : '“ Our task must be to free ourselves... by widening our circle of compassion to embrace all living creatures and the whole ofnature and it's beauty.”',
+    "-Pierce Brown" :'“Man cannot be freed by the same injustice that enslaved it.”',
+    "- Deepak Chopra" :'“Sex is always about emotions. Good sex is about free emotions; bad sex is about blocked emotions.”'
+
+}
+var authors = object.keys(quotes);
+console.log(authors);
+}
